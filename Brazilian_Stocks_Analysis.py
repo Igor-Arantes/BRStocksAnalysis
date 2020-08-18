85486001083e676337f5bfdeca8a7c20e2b103ed
Igor Arantes <igor.l.arantes@gmail.com>
2020-08-18 20:35:59 -0300

    Add analysis by investiment type and percent composition

--- Brazilian_Stocks_Analysis.py
+++ Brazilian_Stocks_Analysis.py
@@ -1,55 +1,55 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[116]:
+# In[1]:
 
 
 # Libraries Importation
 
 
-# In[117]:
+# In[2]:
 
 
 import pandas as pd
 
 
-# In[118]:
+# In[3]:
 
 
 import seaborn as sns
 
 
-# In[119]:
+# In[4]:
 
 
 import matplotlib.pyplot as plt
 
 
-# In[120]:
+# In[5]:
 
 
 #Dada source
 
 
-# In[121]:
+# In[6]:
 
 
 url = "https://docs.google.com/spreadsheets/d/1I93xay8dfxiL25qYohMAFIbh204vHBJSC3iKm_Y-Apc/edit?usp=sharing"
 
 
-# In[122]:
+# In[7]:
 
 
 o = pd.read_html(url, index_col=1, skiprows = 1)
 
 
-# In[123]:
+# In[8]:
 
 
 xlsx_path = ('Carteira Acoes B3.xlsx')
 
 
-# In[124]:
+# In[9]:
 
 
 e = pd.read_excel(xlsx_path)
@@ -61,13 +61,13 @@ e = pd.read_excel(xlsx_path)
 
 
 
-# In[125]:
+# In[10]:
 
 
 #Fitting Data
 
 
-# In[126]:
+# In[11]:
 
 
 stocks_now = o[0]
@@ -82,13 +82,13 @@ stocks_dataframe.rename(columns={'Codigo da Açao':'Ticker', 'Valor Pago': 'Valo
 
 
 
-# In[127]:
+# In[12]:
 
 
 #Pick Sotock by Ticker
 
 
-# In[128]:
+# In[13]:
 
 
 def ticker_location(ticker_desired):
@@ -96,25 +96,25 @@ def ticker_location(ticker_desired):
     
 
 
-# In[129]:
+# In[14]:
 
 
 #Finding the total price and add then to the dataframe
 
 
-# In[130]:
+# In[15]:
 
 
 stocks_dataframe['Valor Total'] = stocks_dataframe['Quantidade'] * stocks_dataframe['Valor_Pago']
 
 
-# In[131]:
+# In[16]:
 
 
 #Find Stock by Ticker
 
 
-# In[132]:
+# In[17]:
 
 
 def find_all_stock(ticker_desired):
@@ -127,13 +127,13 @@ def find_all_stock(ticker_desired):
 
 
 
-# In[133]:
+# In[18]:
 
 
 #Mean Value Stock Price
 
 
-# In[134]:
+# In[19]:
 
 
 def mean_price_paid(ticker_desired):
@@ -141,13 +141,13 @@ def mean_price_paid(ticker_desired):
     return round(x[-1]/x[1],2)
 
 
-# In[135]:
+# In[20]:
 
 
 #Sum of desired stock
 
 
-# In[136]:
+# In[21]:
 
 
 def sum_stock_desired(ticker_desired):
@@ -155,51 +155,51 @@ def sum_stock_desired(ticker_desired):
     return x[1]
 
 
-# In[137]:
+# In[22]:
 
 
 #valuation or devaluation in total
 
 
-# In[138]:
+# In[23]:
 
 
 def valuation(ticker_desired):
     return round((stocks_now.loc[ticker_desired, 'Preco']-mean_price_paid(ticker_desired))*sum_stock_desired(ticker_desired),2)
 
 
-# In[139]:
+# In[24]:
 
 
 #Percent variation
 
 
-# In[140]:
+# In[25]:
 
 
 def percent_variation(ticker_desired):
     return round(((stocks_now.loc[ticker_desired, 'Preco']-mean_price_paid(ticker_desired))/stocks_now.loc[ticker_desired, 'Preco'])*100,2)
 
 
-# In[141]:
+# In[26]:
 
 
 #Geting a series of uniques stocks names
 
 
-# In[142]:
+# In[27]:
 
 
 unique_stocks_ticker = stocks_dataframe['Ticker'].unique()
 
 
-# In[143]:
+# In[28]:
 
 
 #Creating a data frame with the main analysis values
 
 
-# In[144]:
+# In[29]:
 
 
 def main_analisys_values():
@@ -231,29 +231,29 @@ def main_analisys_values():
     return stocks_stats
 
 
-# In[145]:
+# In[30]:
 
 
 stocks_stats = main_analisys_values()
 
 
-# In[146]:
+# In[31]:
 
 
 #Print the results
 
 
-# In[147]:
+# In[32]:
 
 
 print(stocks_stats)
 
 
-# In[148]:
+# In[33]:
 
 
 #Setting green to positive values and red to negative ones
-
+custom_palette={}
 for i in range(0, len(stocks_stats.Valuation)):
     if  stocks_stats.Valuation[i] >= 0:
         custom_palette[stocks_stats.Ticker[i]] = 'g'
@@ -266,10 +266,11 @@ plt.figure(figsize=(15,8))
 sns.barplot(x='Ticker', y='Valuation', data=stocks_stats, palette=custom_palette)
 
 
-# In[149]:
+# In[104]:
 
 
 #Setting green to positive values and red to negative ones
+custom_palette={}
 for i in range(0, len(stocks_stats['Percent variation'])):
     if  stocks_stats['Percent variation'][i] >= 0:
         custom_palette[stocks_stats.Ticker[i]] = 'g'
@@ -278,59 +279,59 @@ for i in range(0, len(stocks_stats['Percent variation'])):
         
 #Ploting percent data
 sns.set(style='whitegrid')
-plt.figure(figsize=(15,8)) 
-sns.barplot(x='Ticker', y='Percent variation', data=stocks_stats, palette=custom_palette)
+plt.figure(figsize=(15,12)) 
+sns.barplot(y='Ticker', x='Percent variation', data=stocks_stats, palette=custom_palette)
 
 
-# In[ ]:
+# In[35]:
 
 
 #Finding the percent composition by ticker in the portifolio 
 
 
-# In[197]:
+# In[36]:
 
 
 stocks_stats['Composition'] = round(stocks_stats['Total Value Now']/stocks_stats['Total Value Now'].sum()*100,2)
 
 
-# In[150]:
+# In[37]:
 
 
 #Finding the portifolio performance overall 
 
 
-# In[181]:
+# In[38]:
 
 
 portifolio_performance = {'Total_Invested':[stocks_stats['Total Paid'].sum()],'Total_Value_Now':[stocks_stats['Total Value Now'].sum()]}
 
 
-# In[182]:
+# In[39]:
 
 
 portifolio_performance = pd.DataFrame(portifolio_performance)
 
 
-# In[188]:
+# In[40]:
 
 
 portifolio_performance['Total_Earn_Or_Loss'] = portifolio_performance.Total_Value_Now - portifolio_performance.Total_Invested 
 
 
-# In[194]:
+# In[41]:
 
 
 portifolio_performance['Total_Percent_Variation'] = round((portifolio_performance.Total_Value_Now/portifolio_performance.Total_Invested - 1)*100,2) 
 
 
-# In[198]:
+# In[42]:
 
 
-stocks_stats
+print(portifolio_performance)
 
 
-# In[274]:
+# In[43]:
 
 
 pie_plt = stocks_stats[['Ticker','Composition']]
@@ -338,10 +339,47 @@ pie_plt.set_index('Ticker', inplace = True)
 pie_plt=pie_plt.groupby('Ticker')[['Composition']].sum()
 
 
-# In[276]:
+# In[44]:
 
 
 pie_plt.plot.pie(y='Composition',figsize=(12,12),shadow=True,autopct = '%1.2f%%')
+
+
+# In[45]:
+
+
+#Segregate by Investment type
+
+
+# In[46]:
+
+
+v=[]
+for i in range(len(stocks_now.loc[stocks_stats['Ticker']][['Classe']])):
+  v.append(stocks_now.loc[stocks_stats['Ticker']][['Classe']].iloc[i].Classe)
+stocks_stats['Classe']=v
+
+
+# In[47]:
+
+
+pie_plt = stocks_stats[['Classe','Composition']]
+pie_plt.set_index('Classe', inplace = True)
+pie_plt=pie_plt.groupby('Classe')[['Composition']].sum()
+pie_plt.plot.pie(y='Composition',figsize=(12,12),shadow=True,autopct = '%1.2f%%')
+
+
+# In[129]:
+
+
+plt.figure(figsize=(10,12)) 
+sns.barplot(y='Total Paid', x='Classe',data=stocks_stats, palette="ch:.5" ,estimator=sum, ci=None)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
